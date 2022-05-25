2b1195f138c32a189d140a917d6b94faa8f6d71e
Andrew Nguyen <nguyeand@uw.edu>
2022-05-25 00:02:35 -0700

    re-attempt to build itch.io live

--- source/Debug.hx
+++ source/Debug.hx
@@ -7,25 +7,25 @@ class Debug
 	/**
 	 * Enables the room select menu.
 	 */
-	public static var ROOM_SELECT = true;
+	public static var ROOM_SELECT = false;
 
 	/**
 	 * Respawn at the same room you died in, as opposed to the last checkpoint.
 	 */
-	public static var RESPAWN_AT_SAME_ROOM = true;
+	public static var RESPAWN_AT_SAME_ROOM = false;
 
 	/**
 	 * Keeps score upon death.
 	 */
-	public static var KEEP_SCORE = true;
+	public static var KEEP_SCORE = false;
 
 	/**
 	 * Enable the metronome-like SFX that plays to the beat.
 	 */
-	public static var PLAY_BEAT = true;
+	public static var PLAY_BEAT = false;
 
 	/**
 	 * Whether to start the game by deleting any existing saved data first.
 	 */
-	public static var DELETE_SAVE = true;
+	public static var DELETE_SAVE = false;
 }
