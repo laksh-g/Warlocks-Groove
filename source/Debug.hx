c8008f599c3a32eb5724e85942bdfbb0545a98a2
Laksh Gupta <lakshgupta2000@gmail.com>
2022-05-31 19:39:00 -0700

    Final build for itr3

--- source/Debug.hx
+++ source/Debug.hx
@@ -7,27 +7,27 @@ class Debug
 	/**
 	 * Enables the room select menu.
 	 */
-	public static var ROOM_SELECT = true;
+	public static var ROOM_SELECT = false;
 
 	/**
 	 * Respawn at the same room you died in, as opposed to the last checkpoint.
 	 */
-	public static var RESPAWN_AT_SAME_ROOM = true;
+	public static var RESPAWN_AT_SAME_ROOM = false;
 
 	/**
 	 * Keeps score upon death.
 	 */
-	public static var KEEP_SCORE = true;
+	public static var KEEP_SCORE = false;
 
 	/**
 	 * Enable the metronome-like SFX that plays to the beat.
 	 */
-	public static var PLAY_BEAT = true;
+	public static var PLAY_BEAT = false;
 
 	/**
 	 * Whether to start the game by deleting any existing saved data first.
 	 */
-	public static var DELETE_SAVE = true;
+	public static var DELETE_SAVE = false;
 
 	/**
 	 * Whether or not to force the game to assign this session to a specific play-testing group for the health
@@ -36,5 +36,5 @@ class Debug
 	 * `1 =` group A: use fixed health pickups
 	 * `2 =` group B: use health pellets dropped on kill
 	 */
-	public static var AB_GROUP = 1;
+	public static var AB_GROUP = 0;
 }
